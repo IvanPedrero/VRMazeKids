e37c4f21af7eb60dd80fd43da711476d0001042a
VictorCano18 <62313025+VictorCano18@users.noreply.github.com>
2020-09-24 23:10:47 -0500

    Nivel 3 - Diseño de nivel y mecánica de juego
    Si pones en su lugar las gemas 3 veces seguidas ganas.
    Hay fallo si eliges 2 o más gemas al mismo tiempo. Hay fallo si te equivocas del lugar de la gema pues no reconoce la condicion en el script.

---
+++ VR Maze Kids/Assets/Scripts/Activities/LearningN_Activity/LearningN3/CameraRayCastController3.cs
@@ -0,0 +1,87 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CameraRayCastController3 : MonoBehaviour
+{
+    public static int score, wrongAnswer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            // Hit something...
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                Transform objectHit = hit.transform;
+                if (objectHit.gameObject.tag == "1" && objectHit.gameObject.name == "gem")
+                {
+                    Destroy(hit.transform.gameObject);
+
+                    if (objectHit.gameObject.tag != "1")
+                    {
+                        score = 0;
+                        SceneManager.LoadScene(3);
+                    }
+                }
+                if (objectHit.gameObject.tag == "2" && objectHit.gameObject.name == "gem")
+                {
+                    Destroy(hit.transform.gameObject);
+
+                    if (objectHit.gameObject.tag == "2")
+                    {
+                        if (wrongAnswer != 0)
+                        {
+                            wrongAnswer = 0;
+                        }
+                        
+                    }
+                    else
+                    {
+                        score = 0;
+                        SceneManager.LoadScene(3);
+                    }
+                }
+                if (objectHit.gameObject.tag == "3" && objectHit.gameObject.name == "gem")
+                {
+                    Destroy(hit.transform.gameObject);
+
+                    if (objectHit.gameObject.tag == "3")
+                    {
+                        if (wrongAnswer != 0)
+                        {
+                            wrongAnswer = 0;
+                        }
+                        score++;
+                        StartCoroutine(reloadScreen());
+                    }
+                    else
+                    {
+                        score = 0;
+                        SceneManager.LoadScene(3);
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    public IEnumerator reloadScreen()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(3);
+    }
+
+}
