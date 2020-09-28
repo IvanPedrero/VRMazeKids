a0177f69848930123883243eecc927949ae3dd49
AngelRebollar <59886227+AngelRebollar@users.noreply.github.com>
2020-09-28 00:10:42 -0500

    Nivel 4 - Completo

---
+++ VR Maze Kids/Assets/Scripts/Activities/LearningN_Activity/LearningN4/BalloonsNumbers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BalloonsNumbers : MonoBehaviour
+{
+    Conversion con = new Conversion();
+    public GameObject B_1, B_2, B_3, B_4, B_5;
+    public GameObject[] B = new GameObject[5];
+    public TextMesh dialogo;
+    private string a, b, c, d, f;
+    public static int respuesta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = UnityEngine.Random.Range(1, 4).ToString();
+        b = UnityEngine.Random.Range(1, 4).ToString();
+        c = UnityEngine.Random.Range(1, 4).ToString();
+        d = UnityEngine.Random.Range(4, 6).ToString();
+        f = UnityEngine.Random.Range(4, 6).ToString();
+
+        while (b == c || b == a || c == a )
+        {
+            if (a == b || a == c)
+            {
+                a = UnityEngine.Random.Range(1, 4).ToString();
+            }
+            if (b == c)
+            {
+                b = UnityEngine.Random.Range(1, 4).ToString();
+            }
+        }
+
+        while (d == f)
+        {
+            d = UnityEngine.Random.Range(4, 6).ToString();
+        }
+
+        B_1.tag = a;
+        B_2.tag = b;
+        B_3.tag = c;
+        B_4.tag = d;
+        B_5.tag = f;
+
+        respuesta = UnityEngine.Random.Range(1,1000);
+        dialogo.text = con.enletras(respuesta.ToString());
+
+        B[0] = B_1;
+        B[1] = B_2;
+        B[2] = B_3;
+        B[3] = B_4;
+        B[4] = B_5;
+
+        for (int i=0; i<5; i++)
+        {
+            if(B[i].tag == "1")
+            {
+                B[i].GetComponentInChildren<TextMesh>().text = ((int)(respuesta / 100)).ToString();
+            }
+
+            if (B[i].tag == "2")
+            {
+                B[i].GetComponentInChildren<TextMesh>().text = ((int)((respuesta % 100) / 10)).ToString();
+            }
+
+            if(B[i].tag == "4")
+            {
+                B[i].GetComponentInChildren<TextMesh>().text = ((int)((respuesta % 100) % 10)).ToString();
+            }
+
+            if(B[i].tag == "3")
+            {
+                B[i].GetComponentInChildren<TextMesh>().text = UnityEngine.Random.Range(0, 10).ToString();
+            }
+
+            if (B[i].tag == "5")
+            {
+                B[i].GetComponentInChildren<TextMesh>().text = UnityEngine.Random.Range(0, 10).ToString();
+            }
+        }
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR Maze Kids/Assets/Scripts/Activities/LearningN_Activity/LearningN4/CameraRayCastController4.cs
@@ -0,0 +1,75 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System;
+
+public class CameraRayCastController4 : MonoBehaviour
+{
+    public static int score, wrongAnswer;
+    private string NCorrecto;
+    private int RCorrecta;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            // Hit something...
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                Transform objectHit = hit.transform;
+
+                if (objectHit.gameObject.tag == "1" || objectHit.gameObject.tag == "2" || objectHit.gameObject.tag == "3" || objectHit.gameObject.tag == "4" || objectHit.gameObject.tag == "5")
+                {
+                    objectHit.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+
+                    NCorrecto = NCorrecto + objectHit.gameObject.GetComponentInChildren<TextMesh>().text;
+                    //Debug.Log(Ncorrecto);
+                }
+
+                if (objectHit.gameObject.tag == "Niño")
+                {
+                    RCorrecta = int.Parse(NCorrecto);
+                    if (RCorrecta == BalloonsNumbers.respuesta)
+                    {
+                        if (wrongAnswer != 0)
+                        {
+                            wrongAnswer = 0;
+                        }
+                        score++;
+                        Debug.Log(score);
+                        StartCoroutine(reloadScreen());
+                    }
+                    else
+                    {
+                        if (score != 0)
+                        {
+                            score = 0;
+                        }
+                        wrongAnswer++;
+                        Debug.Log(score);
+                        StartCoroutine(reloadScreen());
+                    }
+                }
+            }
+        }
+    }
+
+    public IEnumerator reloadScreen()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(4);
+    }
+
+}
